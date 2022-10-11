3a6537cac60cb34763180805e84307ea94d787e8
User <alexust11@yandex.ru>
2022-10-11 07:32:06 +0700

    начало

--- src/Data.java
+++ src/Data.java
@@ -1,23 +1,21 @@
 public class Data {
-//    private String login;
-//    private String password;
-//    private String confirmPassword;
+
+    private static String VALIDAT_CHARACTERS="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789_";
 
     public Data() {
     }
 
     public static boolean validate (String login, String password, String confirmPassword) {
-        try {
-            check(login, password, confirmPassword);
-        } catch (WrongPasswordException e){
-            System.out.println(e.getMessage());
-            return false;
-        }
+      try { check(login,password,confirmPassword);
 
-
-    return true;
+      } catch (WrongPasswordException|WrongLoginException e) {
+          System.out.println(e.getMessage());
+          return false;
+      }
+        return true;
    }
-   private static boolean check(String login, String password, String confirmPassword) {
+   private static void check(String login, String password, String confirmPassword)
+    throws WrongLoginException, WrongPasswordException{
 
    }
 
