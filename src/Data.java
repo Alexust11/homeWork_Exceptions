b8e23e94729eddcfac9d55cdf7bd24ac18db9451
Алексей <alexust11@yandex.ru>
2022-10-12 14:48:34 +0700

    Д/З 1

--- src/Data.java
+++ src/Data.java
@@ -1,6 +1,6 @@
 public class Data {
 
-    private static String VALIDAT_CHARACTERS="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789_";
+    private static final String VALIDAT_CHARACTERS="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789_";
 
     public Data() {
     }
@@ -16,7 +16,28 @@ public class Data {
    }
    private static void check(String login, String password, String confirmPassword)
     throws WrongLoginException, WrongPasswordException{
-
+       if (!validete(login)) {
+           throw new WrongLoginException(" Логин не валидный");
+       }
+       if (!validete(password)) {
+           throw new WrongPasswordException(" Пароль не валидный");
+       }
+       if (!password.equals(confirmPassword)) {
+           throw new WrongPasswordException("Пароли должны совпадать");
+       }
    }
+
+    private static boolean validete(String s) {
+        if (s.length() > 20) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!VALIDAT_CHARACTERS.contains(String.valueOf(s.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+        }
 
 }
